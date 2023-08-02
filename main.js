566535ab067b1ef862d5fdbf946c90649d5d8e9e
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-08-02 13:36:12 +0530

    changes in my portpolio

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 
 var owl = $('.contain');
 owl.owlCarousel({
-    items:4,
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
+    items: 4,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 1
         },
-        1000:{
-            items:2
+        1000: {
+            items: 2
         }
     }
 });
@@ -25,3 +25,17 @@ let toggleBtn = document.querySelector('#nave-bar');
 function btn() {
     toggleBtn.classList.toggle('nav');
 }
+
+
+var typed = new Typed(".auto-type", {
+    strings: ["Front End Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
+
+
+let loader = document.getElementById("loader-wrapper");
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
